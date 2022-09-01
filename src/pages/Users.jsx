2cc9004068119cedf788e050fcb9580c4bb58e78
srikanthiveond <srikanth.k@iveond.com>
2022-09-01 16:08:13 +0530

    card

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,39 +1,39 @@
-import { Button, Container ,  Card ,  Row ,Col} from "react-bootstrap";
+import { Button, Container , Card ,  Row ,Col} from "react-bootstrap";
 import image from "../logo.svg";
 
 function BasicExample() {
   return (
-    <Container>
+    <Container className="justify-content-center">
       <Row>
-        <Col>
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={image} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-    </Col>
+      <Col>
+          <Card style={{ width: '18rem' }}>
+             <Card.Img variant="top" src={image} />
+               <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                   <Card.Text>
+                      Some quick example text to build on the card title and make up the
+                      bulk of the card's content.
+                   </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+          </Card>
+       </Col>
     </Row>
     <br />
     <Row>
-    <Col>
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={image} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-    </Col>
+      <Col md={{ span: 12, offset: 6 }}>{
+          <Card style={{ width: '18rem' }}>
+             <Card.Img variant="top" src={image} />
+               <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                   <Card.Text>
+                      Some quick example text to build on the card title and make up the
+                      bulk of the card's content.
+                   </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+          </Card>}
+       </Col>
     </Row>
     </Container>
   );
